740c9bf7050594ab5d10c1f7943b318cace4a0e4
Emily <lchutian@163.com>
2020-11-11 11:03:47 +0800

    add junit4 testcases

---
+++ testcases/package-test/junit4/oe_test_junit4_compare_different_performances/JunitTestSuite.java
@@ -0,0 +1,22 @@
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
+public class JunitTestSuite {
+   public static void main(String[] a) {
+      long starTime=System.currentTimeMillis();      
+
+      TestSuite suite = new TestSuite(TestJunit1.class, TestJunit3.class);
+      suite.addTestSuite(TestJunit2.class);
+      TestResult result = new TestResult();
+      suite.run(result);
+
+      long endTime=System.currentTimeMillis();
+      long Time=endTime-starTime;
+      System.out.println("JunitTestSuiteTime:"+Time);
+      System.out.println("Number of testSuite cases = " + suite.testCount());
+      System.out.println("Number of test cases = " + result.runCount());
+      suite.setName("testNewName");
+      String newName= suite.getName();
+      System.out.println("Test Suite Name = "+ newName);
+   }
+}
