7f2eb928a5357d3de1016c171d295676587c41d0
the_laughing_man <2115994138@qq.com>
2021-07-13 17:05:13 +0800

    add command test add modify format

--- testcases/cli-test/attr/oe_test_attr.sh
+++ testcases/cli-test/attr/oe_test_attr.sh
@@ -11,7 +11,7 @@
 ####################################
 #@Author    	:   wss1235
 #@Contact   	:   2115994138@qq.com
-#@Date      	:   2021-07-11 14:49:00
+#@Date      	:   2021-07-11 15:01:00
 #@License   	:   Mulan PSL v2
 #@Version   	:   1.0
 #@Desc      	:   command test attr
@@ -22,38 +22,44 @@ source ${OET_PATH}/libs/locallibs/common_lib.sh
 # 测试对象、测试需要的工具等安装准备
 function pre_test()
 {
-    LOG_INFO "Start to prepare the test environment."
-    touch test && ln -s test test.lnk
-    LOG_INFO "End to prepare the test environment."
+	LOG_INFO "Start to prepare the test environment."
+	touch test && ln -s test test.lnk
+	LOG_INFO "End to prepare the test environment."
 }
 
 # 测试点的执行
 function run_test()
 {
-    LOG_INFO "Start to run test."
-    attr -s "oe" -V "top" test
-    CHECK_RESULT $?
-    attr -g "oe" test|grep top
-    CHECK_RESULT $?
-    attr -l test |grep oe
-    CHECK_RESULT $?
-    attr -r oe test
-    CHECK_RESULT $?
-    attr -Lq -s "oe" -V "top" test.lnk
-    CHECK_RESULT $?
-    attr -Rq -s "oe" -V "betop" test
-    CHECK_RESULT $?
-    attr -Sq -s "oe" -V "beentop" test
-    CHECK_RESULT $?
-    LOG_INFO "End to run test."
+	LOG_INFO "Start to run test."	
+	attr -s "oe" -V "top" test
+	CHECK_RESULT $? 0 0 "add attr failed"
+	attr -g "oe" test | grep top
+	CHECK_RESULT $? 0 0 "get attr failed"
+	attr -l test | grep oe
+	CHECK_RESULT $? 0 0 "list attr failed"
+	attr -r oe test
+	CHECK_RESULT $? 0 0 "remove attr failed"
+	attr -Lq -s "oe" -V "top" test.lnk
+	CHECK_RESULT $? 0 0 "add attr by link failed"
+	attr -Rq -s "oe" -V "betop" test
+	CHECK_RESULT $? 0 0 "add attr by root failed"
+	attr -Sq -s "oe" -V "beentop" test
+	CHECK_RESULT $? 0 0 "add attr by securty failed"
+	setfattr -n user.oe -v extra -h test
+	CHECK_RESULT $? 0 0 "set attr failed"
+	getfattr -hRLP -n user.oe -d test
+	CHECK_RESULT $? 0 0 "get attr failed"
+	setfattr -x user.oe -h test
+	CHECK_RESULT $? 0 0 "remove attr failed"
+	LOG_INFO "End to run test."
 }
 
 # 后置处理，恢复测试环境
 function post_test()
 {
-    LOG_INFO "Start to restore the test environment."
-    rm -f test.lnk test
-    LOG_INFO "End to restore the test environment."
+	LOG_INFO "Start to restore the test environment."
+	rm -f test.lnk test
+	LOG_INFO "End to restore the test environment."
 }
 
 main "$@"
