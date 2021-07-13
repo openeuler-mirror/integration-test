50a85b765c2a46d11d7262aa25e35c29a37be240
mcfd <990773468@qq.com>
2021-07-13 11:06:15 +0800

    fix

--- testcases/cli-test/uuid/oe_test_uuidout_01.sh
+++ testcases/cli-test/uuid/oe_test_uuidout_01.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+
 # Copyright (c) 2020. Huawei Technologies Co.,Ltd.ALL rights reserved.
 # This program is licensed under Mulan PSL v2.
 # You can use it according to the terms and conditions of the Mulan PSL v2.
@@ -8,32 +9,23 @@
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
 ####################################
-#@Author    	:   mcfd
-#@Contact   	:   mcfd@qq.com
-#@Date      	:   2021/7/11
+#@Author    	:   mcfdd
+#@Contact   	:   990773468@qq.com
+#@Date      	:   2021-07-13 10:11:00
 #@License   	:   Mulan PSL v2
 #@Version   	:   1.0
-#@Desc      	:   Testing uuid 
+#@Desc      	:   command  test uuid
 #####################################
 
 source ${OET_PATH}/libs/locallibs/common_lib.sh
 
-# 需要预加载的数据、参数配置
-function config_params()
-{
-    LOG_INFO "Start to config params of the case."
-
-    LOG_INFO "No params need to config."
-
-    LOG_INFO "End to config params of the case."
-}
 
 # 测试对象、测试需要的工具等安装准备
 function pre_test()
 {
     LOG_INFO "Start to prepare the test environment."
 
-    dnf install uuid -y
+    DNF_INSTALL "uuid"
 
     LOG_INFO "End to prepare the test environment."
 }
@@ -42,20 +34,35 @@ function pre_test()
 function run_test()
 {
     LOG_INFO "Start to run test."
-
-    # 测试命令：uuid
     uuid
-    CHECK_RESULT 0
-
-    # 测试生成情况
-    STRING=uuid
-    if [ -n "$STRING" ]; then
-
-    CHECK_RESULT 0 
-
-    fi
-
-
+    CHECK_RESULT $?
+    uuid -v1
+    CHECK_RESULT $?
+    uuid -v3 ns:URL https://kernel.org
+    CHECK_RESULT $?
+    uuid -v4
+    CHECK_RESULT $?
+    uuid -v5 ns:URL https://kernel.org
+    CHECK_RESULT $?
+    uuid -m
+    CHECK_RESULT $?
+    uuid -n2
+    CHECK_RESULT $?
+    uuid -n3 -1
+    CHECK_RESULT $?
+    uuid -F bin
+    CHECK_RESULT $?
+    uuid -F str
+    CHECK_RESULT $?
+    uuid -F siv
+    CHECK_RESULT $?
+    uuid -o 1.txt
+    ls | grep 1.txt
+    CHECK_RESULT $?
+    uuid -h
+    CHECK_RESULT $?
+    uuid -d b847136c-e382-11eb-8b80-1b1b93efe3b6
+    CHECK_RESULT $?
     LOG_INFO "End to run test."
 }
 
@@ -64,7 +71,8 @@ function post_test()
 {
     LOG_INFO "Start to restore the test environment."
 
-    LOG_INFO "Need't to restore the tet environment."
+    rm 1.txt
+    DNF_REMOVE "uuid"
 
     LOG_INFO "End to restore the test environment."
 }
