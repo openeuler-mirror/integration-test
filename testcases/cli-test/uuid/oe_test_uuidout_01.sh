b6d4d273a0d2fe95dd220c1947207141b8a36698
mcfd <990773468@qq.com>
2021-07-25 21:45:37 +0800

    up

--- testcases/cli-test/uuid/oe_test_uuidout_01.sh
+++ testcases/cli-test/uuid/oe_test_uuidout_01.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Copyright (c) 2020. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
 # This program is licensed under Mulan PSL v2.
 # You can use it according to the terms and conditions of the Mulan PSL v2.
 #          http://license.coscl.org.cn/MulanPSL2
@@ -9,12 +9,11 @@
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
 ####################################
-#@Author    	:   mcfdd
+#@Author    	:   mcfd
 #@Contact   	:   990773468@qq.com
 #@Date      	:   2021-07-13 10:11:00
 #@License   	:   Mulan PSL v2
-#@Version   	:   1.0
-#@Desc      	:   command  test uuid
+#@Desc      	:   command test uuid
 #####################################
 
 source ${OET_PATH}/libs/locallibs/common_lib.sh
