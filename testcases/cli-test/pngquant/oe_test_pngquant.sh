a3edcada73a85e0889aeab67432938c0540c658d
jhon-hsu <lookforwardxu@163.com>
2021-07-28 15:05:34 +0800

    modify testcase

--- testcases/cli-test/pngquant/oe_test_pngquant.sh
+++ testcases/cli-test/pngquant/oe_test_pngquant.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Copyright (c) 2020. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
 # This program is licensed under Mulan PSL v2.
 # You can use it according to the terms and conditions of the Mulan PSL v2.
 #          http://license.coscl.org.cn/MulanPSL2
@@ -13,13 +13,11 @@
 #@Contact   	:   lookforwardxu@163.com
 #@Date      	:   2021-07-15 09:39:43
 #@License   	:   Mulan PSL v2
-#@Version   	:   1.0
 #@Desc      	:   test command pngquant
 #####################################
 
 source ${OET_PATH}/libs/locallibs/common_lib.sh
 
-# 需要预加载的数据、参数配置
 function config_params()
 {
     LOG_INFO "Start to config params of the case."
@@ -29,20 +27,18 @@ function config_params()
     LOG_INFO "End to config params of the case."
 }
 
-# 测试对象、测试需要的工具等安装准备
 function pre_test()
 {
     LOG_INFO "Start to prepare the test environment."
 
     mkdir output_test
-    wget https://pngquant.org/Ducati_side_shadow.png && mv Ducati_side_shadow.png test.png
-    DNF_INSTALL pngquant
-    DNF_INSTALL libimagequant
+    wget -P ./ https://pngquant.org/Ducati_side_shadow.png && mv Ducati_side_shadow.png test.png
+    cp test.png test-copy.png
+    DNF_INSTALL "pngquant libimagequant"
 
     LOG_INFO "End to prepare the test environment."
 }
 
-# 测试点的执行
 function run_test()
 {
     LOG_INFO "Start to run test."
@@ -50,32 +46,65 @@ function run_test()
     pngquant --help
     CHECK_RESULT $? 0 0 "Failed to run command: pngquant --help"
 
+    pngquant test.png --verbose
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --verbose"
+
+    rm -f ./test-fs8.png
     pngquant test.png
     CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant test.png"
 
+    pngquant - <test.png >test-stdout.png
+    CHECK_RESULT "$(ls | grep -cE 'test-stdout.png')" 1 0 "Failed to test stdin and stdout"
+   
+    rm -f ./test-fs8.png
+    pngquant test.png test-copy.png
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png|test-copy-fs8.png')" 2 0 "Failed to test more than one file"
+
+    rm -f ./test-fs8.png
+    pngquant --posterize 3 test.png
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --posterize"
+
+    rm -f ./test-fs8.png
+    pngquant --strip test.png
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --strip"
+    
     pngquant test.png --ext .demo
     CHECK_RESULT "$(ls | grep -cE 'test.demo')" 1 0 "Failed to run command: pngquant --ext"
 
     pngquant test.png --output ./output_test/test_output.png
     CHECK_RESULT "$(ls ./output_test | grep -cE 'test_output.png')" 1 0 "Failed to run command: pngquant --output"
     
-    rm -rf ./test-fs8.png
+    rm -f ./test-fs8.png
+    pngquant 64 test.png
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to test option [ncolors]"
+
+    pngquant --force test.png
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --force"
+    
+    rm -f ./test-fs8.png
+    pngquant --skip-if-larger test.png
+    CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --skip-if-larger"
+    
+    pngquant --nofs test.png
+    CHECK_RESULT "$(ls | grep -cE 'test-or8.png')" 1 0 "Failed to run command: pngquant --nofs"
+    
+    rm -f ./test-fs8.png
     pngquant test.png --quality 50
     CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --quality"
 
-    rm -rf ./test-fs8.png
+    rm -f ./test-fs8.png
     pngquant test.png --speed 5 
     CHECK_RESULT "$(ls | grep -cE 'test-fs8.png')" 1 0 "Failed to run command: pngquant --speed"
 
     LOG_INFO "End to run test."
 }
 
-# 后置处理，恢复测试环境
 function post_test()
 {
     LOG_INFO "Start to restore the test environment."
-
-    rm -rf test.png test.demo test-fs8.png ./output_test/
+    
+    DNF_REMOVE
+    rm -rf ./test* ./output_test/
 
     LOG_INFO "End to restore the test environment."
 }
